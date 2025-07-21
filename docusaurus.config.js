e41eff303852e2fbba8287f760a410312aee29b6
Burak Sezer <burak.sezer.developer@gmail.com>
2025-07-22 00:10:03 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kronotop', // Usually your GitHub org/user name.
-  projectName: 'kronotop.com', // Usually your repo name.
+  projectName: 'kronotop.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages', // The branch that GitHub pages will deploy from.
 
   trailingSlash: false,
